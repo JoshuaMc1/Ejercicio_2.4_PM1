c255116744df6f736c5252da721fab4792c6e217
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2022-07-15 22:42:46 -0600

    Initial commit

---
+++ app/src/main/java/com/joshuamc/ejercicio2_4/OBJ/Signatures.java
@@ -0,0 +1,27 @@
+package com.joshuamc.ejercicio2_4.OBJ;
+
+public class Signatures {
+    private String descripcion;
+    private byte[] firma;
+    private Integer id;
+
+    public Signatures(Integer id,byte[] firma,String descripcion) {
+        this.id = id;
+        this.firma = firma;
+        this.descripcion = descripcion;
+    }
+
+    public Signatures() {}
+
+    public String getDescripcion() {return descripcion;}
+
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+
+    public byte[] getFirma() {return firma;}
+
+    public void setFirma(byte[] firma) {this.firma = firma;}
+
+    public Integer getId() {return id;}
+
+    public void setId(Integer id) {this.id = id;}
+}
